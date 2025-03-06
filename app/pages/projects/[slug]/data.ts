424b58e173cc1bb1d9f8a1a9c91286951482ec9d
WilliamHankey <wchankey15@gmail.com>
2025-03-06 22:27:54 +0200

    Rotation fixed

--- app/pages/projects/[slug]/data.ts
+++ app/pages/projects/[slug]/data.ts
@@ -1,17 +1,17 @@
 export function getProjects() {
     return [
       {
-        slug: "ai-dashboard",
+        slug: "Synergy Flow",
         title: "AI Dashboard",
         description: "Statsig is a faithful replica of the growth infrastructure used inside Facebookthat allowed it to grow quickly (i.e Gatekeeper, Deltoid, Quick Experiments, Unidash, Scuba). Our mission is to become all-in-one data platform for allproduct builders and we have three key pillars of our product that supports it:Feature Flagging, Experimentation and Analytics.",
         image: "/assests/projects/synergyflow.svg",
         icons: ["devicon-react-original", "devicon-nodejs-plain"],
       },
       {
-        slug: "ecommerce-app",
+        slug: "Reguhub",
         title: "E-Commerce App",
         description: "A modern e-commerce platform with seamless UI/UX.",
-        image: "/assests/projects/statsig.png",
+        image: "/assests/projects/reguhub.png",
         icons: ["devicon-nextjs-original", "devicon-tailwindcss-plain"],
       },
       {
